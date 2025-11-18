b8d7a848e45bd7360518c0326c8d793bcebc1f14
Tony-MCL <MorningCoffeeLabs@gmail.com>
2025-11-18 14:15:53 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,11 +4,12 @@ const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
   reactStrictMode: true,
-  // Viktig for GitHub Pages: genererer statiske HTML-filer i ./out
   output: "export",
-  // Viktig for prosjekt-sider: appen ligger under /website_V2
   basePath: isProd ? "/website_V2" : "",
-  assetPrefix: isProd ? "/website_V2/" : ""
+  assetPrefix: isProd ? "/website_V2/" : "",
+  env: {
+    NEXT_PUBLIC_BASE_PATH: isProd ? "/website_V2" : ""
+  }
 };
 
 export default nextConfig;
