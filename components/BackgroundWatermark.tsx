02ecca12b64f557812093b86e636866b3a9fef18
Tony-MCL <MorningCoffeeLabs@gmail.com>
2025-11-18 14:35:10 +0100

    Update BackgroundWatermark.tsx

--- components/BackgroundWatermark.tsx
+++ components/BackgroundWatermark.tsx
@@ -1,11 +1,10 @@
-const basePath = process.env.NEXT_PUBLIC_BASE_PATH ?? "";
-
 export function BackgroundWatermark() {
   return (
     <div className="watermark-layer" aria-hidden="true">
       <img
-        src={`${basePath}/images/mcl-watermark.png`}
+        src={`${process.env.NEXT_PUBLIC_BASE_PATH}/images/mcl-watermark.svg`}
         alt=""
+        className="watermark-svg"
       />
     </div>
   );
